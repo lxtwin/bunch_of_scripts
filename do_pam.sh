68cf4be4c59a3d49a4d03b6beb5ce329f53f10f6
Paul <lxtwin@btinternet.com>
2020-08-19 22:16:24 +0100

    Update do_pam.sh
    Added values for root

--- do_pam.sh
+++ do_pam.sh
@@ -1,21 +1,28 @@
 cp /etc/pam.d/system-auth .
 cp /etc/pam.d/password-auth .
 
+_Attempts="5"
+_Timeout="180"
+_RootTimeout="300"
+
+
 # The auth string to use.
-_auth="\
+_Auth="\
 ### START CIS Update ###\n\
-auth        required      pam_faillock.so preauth silent audit deny=5 even_deny_root unlock_time=180 root_unlock_time=180\n\
-auth        [default=die] pam_faillock.so authfail audit deny=5 unlock_time=180\n\
+auth        required      pam_faillock.so preauth silent audit deny=${_Attempts} even_deny_root unlock_time=180 root_unlock_time=${_RootTimeout}\n\
+auth        [default=die] pam_faillock.so authfail audit deny=${_Attempts} unlock_time=${_Timeout}\n\
 ### END CIS Update ###"
 
 # The account string to use.
-_account="\
+_Account="\
 ### START CIS Update ###\n\
 account     required      pam_faillock.so\n\
 ### END CIS Update ###"
 
 
-sed -i.sys_bkp  -e '/^\s*auth\s*required\s*pam_deny/ a'"${_auth}"'' -e '/^\s*account\s*required\s*pam_permit/ a'"${_account}"'' ./system-auth
-sed -i.pass_bkp -e '/^\s*auth\s*required\s*pam_deny/ a'"${_auth}"'' -e '/^\s*account\s*required\s*pam_permit/ a'"${_account}"'' ./password-auth
+sed -i.sys_bkp  -e '/^\s*auth\s*required\s*pam_deny/ a'"${_Auth}"'' -e '/^\s*account\s*required\s*pam_permit/ a'"${_Account}"'' ./system-auth
+sed -i.pass_bkp -e '/^\s*auth\s*required\s*pam_deny/ a'"${_Auth}"'' -e '/^\s*account\s*required\s*pam_permit/ a'"${_Account}"'' ./password-auth
 
 exit
+
+

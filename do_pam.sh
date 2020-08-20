3406cdb6a3619e19e273ecd4054b266608ede88d
Paul <lxtwin@btinternet.com>
2020-08-20 16:03:56 +0100

    Update do_pam.sh
    cleaner

--- do_pam.sh
+++ do_pam.sh
@@ -7,22 +7,27 @@ _RootTimeout="300"
 
 
 # The auth string to use.
-_Auth="\
-### START CIS Update ###\n\
-auth        required      pam_faillock.so preauth silent audit deny=${_Attempts} even_deny_root unlock_time=180 root_unlock_time=${_RootTimeout}\n\
-auth        [default=die] pam_faillock.so authfail audit deny=${_Attempts} unlock_time=${_Timeout}\n\
-### END CIS Update ###"
+_Auth1="\
+### CIS Update 1 ###\n\
+auth        required      pam_faillock.so preauth silent audit deny=${_Attempts} even_deny_root unlock_time=180 root_unlock_time=${_RootTimeout}"
+
+_Auth2="\
+### CIS Update 2 ###\n\
+auth        [default=die] pam_faillock.so authfail audit deny=${_Attempts} unlock_time=${_Timeout}"
 
 # The account string to use.
 _Account="\
-### START CIS Update ###\n\
-account     required      pam_faillock.so\n\
-### END CIS Update ###"
+### CIS Update 3 ###\n\
+account     required      pam_faillock.so"
 
 
-sed -i.sys_bkp  -e '/^\s*auth\s*required\s*pam_deny/ a'"${_Auth}"'' -e '/^\s*account\s*required\s*pam_permit/ a'"${_Account}"'' ./system-auth
-sed -i.pass_bkp -e '/^\s*auth\s*required\s*pam_deny/ a'"${_Auth}"'' -e '/^\s*account\s*required\s*pam_permit/ a'"${_Account}"'' ./password-auth
+sed -i.sys_bkp  -e '/^\s*auth\s*required\s*pam_env.so/ a'"${_Auth1}"'' \
+                -e '/^\s*auth\s*sufficient\s*pam_unix.so/ a'"${_Auth2}"'' \
+                -e '/^\s*account\s*required\s*pam_unix.so/ i'"${_Account}"'' ./system-auth
+
+
+sed -i.pass_bkp -e '/^\s*auth\s*required\s*pam_env.so/ a'"${_Auth1}"'' \
+                -e '/^\s*auth\s*sufficient\s*pam_unix.so/ a'"${_Auth2}"'' \
+                -e '/^\s*account\s*required\s*pam_unix.so/ i'"${_Account}"'' ./password-auth
 
 exit
-
-
